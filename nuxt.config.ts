69411b0f010c0e9077ef1744cdf6dc65f49281a5
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-07-07 13:34:32 +0200

    Deploy auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-vuefire"],
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyBMKBwPbOIX18UZ53B622VgLk4aRmBl1q0",
+      authDomain: "calllogadas2-9a345.firebaseapp.com",
+      databaseURL:
+        "https://calllogadas2-9a345-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "calllogadas2-9a345",
+      storageBucket: "calllogadas2-9a345.appspot.com",
+      messagingSenderId: "466886593574",
+      appId: "1:466886593574:web:f0f49becbd2e4e5a2d6ae7",
+    },
+  },
+});
