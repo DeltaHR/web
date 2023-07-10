f510bd7c77134d890713ddbc84755e898b70f1ed
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-07-10 14:02:13 +0200

    DataTables

---
+++ components/DataTableClient.vue
@@ -0,0 +1,39 @@
+<script setup>
+import DataTable from "datatables.net-vue3";
+import DataTablesLib from "datatables.net";
+import { collection, query, limit } from "firebase/firestore";
+import { useCollection, useFirestore } from "vuefire";
+const db = useFirestore();
+DataTable.use(DataTablesLib);
+// automatically waits for the data to be loaded on the server
+const calls = useCollection(query(collection(db, "call_logs"), limit(3)));
+let letCallsFormat = [];
+console.log(calls);
+// calls.value.forEach((element) => {
+//   console.log(element);
+//   letCallsFormat[index] = element;
+// });
+
+const dat = [
+  [1, 2],
+  [3, 4],
+];
+</script>
+
+<template>
+  <div>
+    <ClientOnly>
+      <DataTable :data="dat" class="display" :options="{ select: true }">
+        <thead>
+          <tr>
+            <th>First</th>
+            <th>Second</th>
+            <th>Second</th>
+            <th>Second</th>
+            <th>Second</th>
+          </tr>
+        </thead>
+      </DataTable>
+    </ClientOnly>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,9 @@
-<script setup>
-
-import { collection, query, limit } from "firebase/firestore";
-import { useFirestore } from "vuefire";
-const db = useFirestore();
-// automatically waits for the data to be loaded on the server
-const calls = useCollection(query(collection(db, "call_logs"), limit(5)));
-</script>
+<script setup></script>
 
 <template>
   <div>
-    <DataTable class="display" :options="{ select: true }">
-      <thead>
-        <tr>
-          <th>First</th>
-          <th>Second</th>
-          <th>Second</th>
-          <th>Second</th>
-          <th>Second</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="call in calls" :key="call.deviceName">
-          <td>{{ call.duration }}</td>
-          <td>{{ call.type }}</td>
-          <td>{{ call.date }}</td>
-          <td>{{ call.deviceName }}</td>
-          <td>{{ call.number }}</td>
-        </tr>
-      </tbody>
-    </DataTable>
+    <ClientOnly>
+      <DataTableClient></DataTableClient>
+    </ClientOnly>
   </div>
 </template>
