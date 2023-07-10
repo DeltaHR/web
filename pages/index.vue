93a3f68bbfbb75d71db0e0be3edb1eb87105241d
Triloworld <2671726+Triloworld@users.noreply.github.com>
2023-07-10 13:24:27 +0200

    DataTables

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,33 @@
 <script setup>
+
 import { collection, query, limit } from "firebase/firestore";
 import { useFirestore } from "vuefire";
-const user = useCurrentUser();
 const db = useFirestore();
 // automatically waits for the data to be loaded on the server
-const calls = useCollection(query(collection(db, "call_logs"), limit(100)));
+const calls = useCollection(query(collection(db, "call_logs"), limit(5)));
 </script>
 
 <template>
-  <ul>
-    <li>{{ user }}</li>
-    <li v-for="call in calls" :key="call.deviceName">
-      <span>{{ call.duration }}</span>
-      <span>{{ call.type }}</span>
-      <span>{{ call.date }}</span>
-      <span>{{ call.deviceName }}</span>
-      <span>{{ call.number }}</span>
-    </li>
-  </ul>
+  <div>
+    <DataTable class="display" :options="{ select: true }">
+      <thead>
+        <tr>
+          <th>First</th>
+          <th>Second</th>
+          <th>Second</th>
+          <th>Second</th>
+          <th>Second</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="call in calls" :key="call.deviceName">
+          <td>{{ call.duration }}</td>
+          <td>{{ call.type }}</td>
+          <td>{{ call.date }}</td>
+          <td>{{ call.deviceName }}</td>
+          <td>{{ call.number }}</td>
+        </tr>
+      </tbody>
+    </DataTable>
+  </div>
 </template>
