b54158c80f23a4664e6691171ca0e314c72ec7b3
Kamil <k.medrala99@gmail.com>
2023-07-12 14:41:38 +0200

    calls disabled temporarly

--- components/DataTableClient.vue
+++ components/DataTableClient.vue
@@ -4,7 +4,9 @@
       <DataTable v-if="calls.length > 0" :data="computedCalls" class="display" :options="{ select: true }">
         <thead>
           <tr>
-            <th v-for="key,index in Object.keys(calls[0]).sort()" :key="index">
+            <!-- <th v-for="key,index in Object.keys(calls[0]).sort()" :key="index">
+            {{ key }}</th> -->
+            <th v-for="key,index in ['a','b','c','d','e']" :key="index">
             {{ key }}</th>
           </tr>
         </thead>
@@ -18,25 +20,26 @@
   import DataTablesLib from "datatables.net";
   import { collection, query, limit } from "firebase/firestore";
   import { useCollection, useFirestore } from "vuefire";
-  const db = useFirestore();
+  // const db = useFirestore();
   DataTable.use(DataTablesLib);
   // automatically waits for the data to be loaded on the server
-  const calls = useCollection(query(collection(db, "call_logs"), limit(1000)));
+  // const calls = useCollection(query(collection(db, "call_logs"), limit(1000)));
 
   const computedCalls = computed(()=>{
-    let formattedData = []
-    if (calls.value) {
-      calls.value.forEach((call)=>{
-        let row = [
-          call.date,
-          call.deviceName,
-          call.duration,
-          call.number,
-          call.type
-        ]
-        formattedData.push(row)
-      })
-    }
+    let formattedData = [[1,2,3,4,5],[6,7,8,9,10]]
+    // let formattedData = []
+    // if (calls.value) {
+    //   calls.value.forEach((call)=>{
+    //     let row = [
+    //       call.date,
+    //       call.deviceName,
+    //       call.duration,
+    //       call.number,
+    //       call.type
+    //     ]
+    //     formattedData.push(row)
+    //   })
+    // }
     return formattedData
   })
 </script>
